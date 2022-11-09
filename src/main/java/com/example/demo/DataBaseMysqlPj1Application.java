130ffe334f20a2c3eedfc41bac5fa974b8fe29c5
Wang HoEun <hoeunwang@gmail.com>
2022-11-09 17:16:51 +0900

    feat: 초기 폴더 생성

---
+++ src/main/java/com/example/demo/DataBaseMysqlPj1Application.java
@@ -0,0 +1,13 @@
+package main.java.com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseMysqlPj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBaseMysqlPj1Application.class, args);
+	}
+
+}
